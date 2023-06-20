971654525df07e497d2a44b5fa6b7ff2e3adc67f
Rafael William <rafael.fim@hotmail.com>
2023-06-20 13:50:50 -0300

    tasks api

---
+++ src/routes.js
@@ -0,0 +1,110 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+function actualDate() {
+  return new Date().toLocaleTimeString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZone: 'America/Sao_Paulo'
+  })
+}
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+      const tasks = database.select(
+        'tasks',
+        search
+          ? {
+              title: search,
+              description: search
+            }
+          : null
+      )
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: actualDate(),
+        updated_at: actualDate()
+      }
+
+      database.insert('tasks', task)
+
+      return res.end('Criação de tarefas')
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end()
+      }
+
+      database.update('tasks', id, {
+        ...task,
+        title,
+        description,
+        updated_at: actualDate()
+      })
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'PATCH',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end()
+      }
+
+      database.update('tasks', id, {
+        ...task,
+        title,
+        description,
+        updated_at: actualDate()
+      })
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      database.delete('tasks', id)
+      return res.writeHead(204).end()
+    }
+  }
+]
